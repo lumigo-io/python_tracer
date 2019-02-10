b1d9c0c82f94efd53884fa0725b2d0bf8f0c7eb0
saartochner <saart@lumigo.io>
2019-02-10 18:58:09 +0200

    WIP

--- src/lumigo_tracer/__init__.py
+++ src/lumigo_tracer/__init__.py
@@ -1,2 +1,6 @@
 from lumigo_tracer import reporter  # noqa
+from lumigo_tracer.libs import xmltodict  # noqa
+from lumigo_tracer.libs import wrapt  # noqa
+from lumigo_tracer.parsers import utils  # noqa
+from lumigo_tracer.parsers import parser  # noqa
 from lumigo_tracer import parsers  # noqa
