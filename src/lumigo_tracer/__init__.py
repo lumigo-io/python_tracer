9fac917c9c26cb1e3a783923741c8c324429dc77
saartochner <saart@lumigo.io>
2019-02-10 18:18:05 +0200

    WIP

--- src/lumigo_tracer/__init__.py
+++ src/lumigo_tracer/__init__.py
@@ -1,2 +1,2 @@
-from lumigo_tracer.sync_http.sync_hook import lumigo_lambda  # noqa
-from lumigo_tracer.async_http.async_hook import lumigo_async_lambda  # noqa
+from .sync_http.sync_hook import lumigo_lambda  # noqa
+from .async_http.async_hook import lumigo_async_lambda  # noqa
