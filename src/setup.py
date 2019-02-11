48b36b350d0433054754dc86ad4c17e69538ef0d
saartochner <saart@lumigo.io>
2019-02-11 13:27:40 +0200

    WIP

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("../README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -11,9 +11,8 @@ setuptools.setup(
     description="Troubleshoot your lambda using lumigo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lumigo-io/lumigo_tracer.git",
-    package_dir={"": "src"},
-    packages=["lumigo_tracer"],
+    url="https://github.com/lumigo-io/python_tracer.git",
+    packages=setuptools.find_packages(exclude="test"),
     install_requires=["aiohttp", "boto3"],
     classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
 )
