4d243a04aa9da00e9c21fb2b48a802e8d38aaec7
saartochner <saart@lumigo.io>
2019-02-19 13:10:11 +0200

    WIP

--- scripts/ci_deploy.sh
+++ scripts/ci_deploy.sh
@@ -1,44 +1,31 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
+# A script being used by circle ci. Should not be ran locally.
 
+# Always delete the stack
+function _finally {
+    cd ~/lumigo-api
+    ./scripts/remove.sh --stage integrationIt --region ${region}
+    cd ~/repo
+    ./scripts/remove.sh --stage integrationIt --region ${region}
+}
 
-echo ".____                  .__                  .__        ";
-echo "|    |    __ __  _____ |__| ____   ____     |__| ____  ";
-echo "|    |   |  |  \/     \|  |/ ___\ /  _ \    |  |/  _ \ ";
-echo "|    |___|  |  /  Y Y  \  / /_/  >  <_> )   |  (  <_> )";
-echo "|_______ \____/|__|_|  /__\___  / \____/ /\ |__|\____/ ";
-echo "        \/           \/  /_____/         \/            ";
-echo
-echo "Deploy to integration environment"
+trap _finally EXIT
 
-branch_name=$(echo ${CIRCLE_BRANCH} | cut -c1-8 | awk '{print tolower($0)}')
-user_name=$(echo ${CIRCLE_USERNAME} | cut -c1-3 | awk '{print tolower($0)}')
-aws_region=us-east-1
-# For CircleCI
-echo "export AWS_DEFAULT_REGION=${aws_region}" >> $BASH_ENV
-if [[ "$branch_name" == "master" ]]; then
-    branch_name=${branch_name}-$(echo ${CIRCLE_SHA1}|cut -c1-4)
-fi
-echo "export USER=int-${branch_name}-${user_name}" >> $BASH_ENV
-echo "Branch: $branch_name"
-echo "User: $user_name"
+export USER=integration
 
+region=us-east-1
 pushd ..
 git clone git@github.com:lumigo-io/lumigo-api.git
+git clone git@github.com:lumigo-io/python-tracer.git
 popd
 
 pushd ../lumigo-api
-./scripts/deploy.sh --encrypted-file credentials_integration.enc --stage integrationIt --region $aws_region
+./scripts/deploy.sh --encrypted-file credentials_integration.enc --stage integrationIt --region ${region}
 popd
 
-function deploy() {
-    echo "${bold}Deploying ../src/test${normal}"
-    pushd ./src/test > /dev/null
-    echo "1"
-    npm i > /dev/null 2>&1
-    echo "2"
-    sls deploy --force --env $env --region $aws_region
-    echo "3"
-}
+pushd ../python-tracer
+python setup.py install
+popd
 
-deploy
+./scripts/deploy.sh --stage integrationIt --region ${region} --stage-backend integrationIt --force
