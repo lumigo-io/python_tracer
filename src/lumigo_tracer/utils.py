b0a7a7d37608404cdaba1c74d459a164ec2cd87d
saartochner <saart@lumigo.io>
2019-03-05 20:23:45 +0200

    RD233-update_default_edge host

--- src/lumigo_tracer/utils.py
+++ src/lumigo_tracer/utils.py
@@ -5,7 +5,7 @@ import urllib.request
 from urllib.error import URLError
 from typing import Union, List
 
-EDGE_HOST = "{region}.lumigo-tracer-edge.golumigo.com"
+EDGE_HOST = "https://{region}.lumigo-tracer-edge.golumigo.com/api/spans"
 LOG_FORMAT = "#LUMIGO# - %(asctime)s - %(levelname)s - %(message)s"
 SHOULD_REPORT = True
 SECONDS_TO_TIMEOUT = 0.3
