9765c6ee15b3428b52cba7c9e65372d9afc68d56
saartochner <47108628+saartochner@users.noreply.github.com>
2019-03-13 19:25:51 +0200

    Publish layer (#17)
    * upload layers when deploy

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -39,6 +39,21 @@ upload_file=$(ls ./dist/*.gz)
 curl -F package=@${upload_file} https://${FURY_AUTH}@push.fury.io/lumigo/
 popd > /dev/null 2>&1
 
+echo "Create Layer"
+mkdir python
+cp -R lumigo_tracer.egg-info python/
+cp -R lumigo_tracer python/
+cp -R VERSION python/
+zip -r lumigo_tracer.zip python
+
+regions=("ap-northeast-1" "ap-northeast-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-north-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2")
+for region in "${regions[@]}"; do
+    version=$(aws lambda publish-layer-version --layer-name lumigo-python-tracer --description "Serverless Troubleshooting Make Simple" --license-info "Apache License Version 2.0" --zip-file fileb://lumigo_tracer.zip --compatible-runtimes python3.6 python3.7 --region ${region}| jq -r '.Version')
+    aws lambda add-layer-version-permission --layer-name lumigo-python-tracer --statement-id engineering-org --principal "*" --action lambda:GetLayerVersion --version-number ${version} --region ${region}
+    echo "published version ${version} to region ${region}"
+done
+
 echo "Create release tag"
 push_tags
+
 echo "Done"
