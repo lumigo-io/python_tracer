e9b26d12a3b7b36aa4baf09bb162def308e7e21f
Efi Merdler-Kravitz <efi@lumigo.io>
2019-05-19 23:26:56 +0300

    Test fail

--- src/test/unit/sync_http/test_sync_hook.py
+++ src/test/unit/sync_http/test_sync_hook.py
@@ -122,9 +122,9 @@ def test_wrapping_with_print_override():
     with CaptureOutput() as capturer:
         assert lambda_test_function({}, SimpleNamespace(aws_request_id="1234")) == 1
         assert utils._ENHANCE_PRINT is True
-        assert any(
-            line == "RequestId: 1234 hello\nRequestId: 1234 world" for line in capturer.get_lines()
-        )
+        assert "RequestId: 1234 hello" in capturer.get_lines()
+        assert "RequestId: 1234 world" in capturer.get_lines()
+
 
 
 def test_wrapping_without_print_override():
