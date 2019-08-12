00ae617f7c4e218cd24592f463eafb71cfbea413
saartochner <47108628+saartochner@users.noreply.github.com>
2019-08-12 18:06:19 +0300

    RD-1051 - feat: add lambda-apigw-lambda support (#73)
    * feat: add lambda-apigw-lambda support

---
+++ src/test/unit/parsers/test_parser.py
@@ -0,0 +1,13 @@
+from lumigo_tracer.parsers.parser import ServerlessAWSParser, Parser
+import http.client
+
+
+def test_serverless_aws_parser_fallback_doesnt_change():
+    url = "https://kvpuorrsqb.execute-api.us-west-2.amazonaws.com"
+    headers = http.client.HTTPMessage()
+    headers.add_header("nothing", "relevant")
+    serverless_parser = ServerlessAWSParser().parse_response(url, 200, headers=headers, body=b"")
+    root_parser = Parser().parse_response(url, 200, headers=headers, body=b"")
+    serverless_parser.pop("ended")
+    root_parser.pop("ended")
+    assert serverless_parser == root_parser
