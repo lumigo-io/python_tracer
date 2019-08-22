3408eb6ae2d769bc9eda846aff0762b2adbddd36
saartochner <saart@lumigo.io>
2019-08-22 09:46:03 +0300

    WIP

--- src/lumigo_tracer/libs/xmltodict.py
+++ src/lumigo_tracer/libs/xmltodict.py
@@ -25,6 +25,11 @@ except NameError:  # pragma no cover
     _unicode = str
 
 
+__author__ = "Martin Blech"
+__version__ = "0.11.0"
+__license__ = "MIT"
+
+
 """
 Copyright (C) 2012 Martin Blech and individual contributors.
 
@@ -35,13 +40,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-
 "Makes working with XML feel like you are working with JSON"
-
-
-__author__ = "Martin Blech"
-__version__ = "0.11.0"
-__license__ = "MIT"
 
 
 class ParsingInterrupted(Exception):
