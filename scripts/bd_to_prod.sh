930f2280ccef87d2ef91231f466dacdcb12d1295
saartochner <47108628+saartochner@users.noreply.github.com>
2020-08-09 17:27:07 +0300

    sync tags and pypi (#135)
    * sync tags and pypi

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -34,24 +34,23 @@ echo "Creating new credential files"
 mkdir -p ~/.aws
 echo ${KEY} | gpg --batch -d --passphrase-fd 0 ${enc_location} > ~/.aws/credentials
 
-echo "Creating lumigo-python-tracer layer"
-./scripts/prepare_layer_files.sh
-
 echo "Getting latest changes from git"
 changes=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline)
 
+sudo pip install --upgrade bumpversion
+bumpversion patch --message "{current_version} → {new_version}. Changes: ${changes}"
+
+push_tags
 
 echo "Uploading to PyPi"
 pip install twine
 twine upload dist/*
 
-push_tags
+echo "Creating lumigo-python-tracer layer"
+./scripts/prepare_layer_files.sh
 
 echo "Creating layer latest version arn table md file (LAYERS.md)"
 ../utils/common_bash/create_layer.sh --layer-name lumigo-python-tracer --region ALL --package-folder python --version $(git describe --abbrev=0 --tags) --runtimes "python3.6 python3.7 python3.8"
-
-sudo pip install --upgrade bumpversion
-bumpversion patch --message "{current_version} → {new_version}. Changes: ${changes}"
 
 cd ../larn && npm i -g
 larn -r python3.6 -n layers/LAYERS36 --filter lumigo-python-tracer -p ~/python_tracer
