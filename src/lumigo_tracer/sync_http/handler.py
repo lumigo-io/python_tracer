294d8e3be06a3958b1ab6b00776236b79ec97c46
saartochner <saart@lumigo.io>
2020-08-18 18:55:23 +0300

    auto-instrument: import the user handler during import

--- src/lumigo_tracer/sync_http/handler.py
+++ src/lumigo_tracer/sync_http/handler.py
@@ -6,18 +6,36 @@ from lumigo_tracer import lumigo_tracer
 ORIGINAL_HANDLER_KEY = "LUMIGO_ORIGINAL_HANDLER"
 
 
-@lumigo_tracer()
-def _handler(*args, **kwargs):
+def parse_handler():
     try:
         module_name, unit_name = os.environ[ORIGINAL_HANDLER_KEY].rsplit(".", 1)
-        importable_name = module_name.replace("/", ".")
-        original_handler = getattr(importlib.import_module(importable_name), unit_name)
-    except (ImportError, AttributeError):
-        raise ImportError(
-            "Could not load the original handler. Are you sure that the import is ok?"
-        )
     except KeyError:
         raise ValueError(
-            "Could not find the original handler. Please follow lumigo's docs: https://docs.lumigo.io/"
+            "Could not find the original handler. Please contact Lumigo for more information."
+        )
+    except ValueError:
+        raise RuntimeError(
+            f"Invalid handler format: Bad handler '{os.environ[ORIGINAL_HANDLER_KEY]}': not enough values to unpack (expected 2, got 1)"
+        )
+    importable_name = module_name.replace("/", ".")
+    return importable_name, unit_name
+
+
+@lumigo_tracer()
+def _handler(*args, **kwargs):
+    handler_module = ""
+    try:
+        handler_module, unit_name = parse_handler()
+        original_handler = getattr(importlib.import_module(handler_module), unit_name)
+    except (ImportError, AttributeError):
+        raise ImportError(
+            f"Unable to import module '{handler_module}': No module named '{handler_module}'"
         )
     return original_handler(*args, **kwargs)
+
+
+try:
+    # import handler during runtime initialization, as usual.
+    importlib.import_module(parse_handler()[0])
+except Exception:
+    pass
