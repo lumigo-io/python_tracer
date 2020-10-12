188bf41a9951e03a662c8a6b80d481ef59b0fc4a
saartochner <saart@lumigo.io>
2020-10-12 19:04:53 +0300

    wip

--- src/test/unit/wrappers/test_no_wrapping_library.py
+++ src/test/unit/wrappers/test_no_wrapping_library.py
@@ -5,9 +5,12 @@ import lumigo_tracer
 
 
 def test_wrapping_without_libraries(monkeypatch):
-    # remove pymongo from path and reload module
+    # remove library from path and reload module
     monkeypatch.setitem(sys.modules, "pymongo", None)
     wrapper = importlib.reload(lumigo_tracer.wrappers.pymongo.pymongo_wrapper)
     assert wrapper.LumigoMongoMonitoring is None
 
     lumigo_tracer.wrappers.wrap()  # should succeed
+
+    monkeypatch.undo()
+    importlib.reload(lumigo_tracer.wrappers.pymongo.pymongo_wrapper)
