df61e658ba676157ea08b8e5ab8f32e42519e3ad
saartochner <saart@lumigo.io>
2020-10-12 13:57:54 +0300

    fix Dori's CR

--- src/test/unit/wrappers/test_no_wrapping_library.py
+++ src/test/unit/wrappers/test_no_wrapping_library.py
@@ -1,12 +1,13 @@
 import importlib
-
 import sys
+
+import lumigo_tracer
 
 
 def test_wrapping_without_libraries(monkeypatch):
+    # remove pymongo from path and reload module
     monkeypatch.setitem(sys.modules, "pymongo", None)
+    wrapper = importlib.reload(lumigo_tracer.wrappers.pymongo.pymongo_wrapper)
+    assert wrapper.LumigoMongoMonitoring is None
 
-    wrapper = importlib.import_module("src.lumigo_tracer.wrappers")
-    wrapper.wrap()  # should succeed
-
-    assert wrapper.pymongo.pymongo_wrapper.LumigoMongoMonitoring is None
+    lumigo_tracer.wrappers.wrap()  # should succeed
