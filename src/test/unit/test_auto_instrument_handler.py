a5475600123f8bb61ab43bf6f6022ef87f65af87
saartochner <47108628+saartochner@users.noreply.github.com>
2021-01-05 13:57:37 +0200

    RD-4296 - mimic AWS import strategy of the original handler (#175)
    * mimic boostrap.py import strategy of the original handler

--- src/test/unit/test_auto_instrument_handler.py
+++ src/test/unit/test_auto_instrument_handler.py
@@ -32,33 +32,72 @@ def test_hierarchy_happy_flow(monkeypatch):
 def test_import_error(monkeypatch):
     monkeypatch.setenv(ORIGINAL_HANDLER_KEY, "blabla.not.exists")
 
-    with pytest.raises(ImportError):
+    try:
         _handler({}, {})
+    except ImportError as e:
+        # Note: We're not using pytest.raises in order to get the exception context
+        assert "Runtime.ImportModuleError" in str(e)
+        assert "another exception occurred" not in traceback.format_exc()
+    else:
+        assert False
 
 
 def test_no_env_handler_error(monkeypatch):
-    if os.environ.get(ORIGINAL_HANDLER_KEY):
-        monkeypatch.delenv(ORIGINAL_HANDLER_KEY)
+    monkeypatch.delenv(ORIGINAL_HANDLER_KEY, None)
 
-    with pytest.raises(ValueError):
+    with pytest.raises(Exception) as e:
         _handler({}, {})
+    assert "Could not find the original handler" in str(e.value)
 
 
 def test_error_in_original_handler_no_extra_exception_log(monkeypatch, context):
-    monkeypatch.setattr(importlib, "import_module", mock.Mock(side_effect=Exception))
+    monkeypatch.setattr(importlib, "import_module", mock.Mock(side_effect=ZeroDivisionError))
     monkeypatch.setenv(ORIGINAL_HANDLER_KEY, "sys.exit")
-    exception_occurred = False
 
     try:
         _handler({}, context)
-    except Exception:
-        exception_occurred = True
+    except ZeroDivisionError:
+        # Note: We're not using pytest.raises in order to get the exception context
         assert "another exception occurred" not in traceback.format_exc()
-    assert exception_occurred is True
+    else:
+        assert False
+
+
+def test_error_in_original_handler_syntax_error(monkeypatch, context):
+    monkeypatch.setattr(importlib, "import_module", mock.Mock(side_effect=SyntaxError))
+    monkeypatch.setenv(ORIGINAL_HANDLER_KEY, "sys.exit")
+
+    try:
+        _handler({}, context)
+    except SyntaxError as e:
+        # Note: We're not using pytest.raises in order to get the exception context
+        assert "Runtime.UserCodeSyntaxError" in str(e)
+        assert "another exception occurred" not in traceback.format_exc()
+    else:
+        assert False
 
 
 def test_handler_bad_format(monkeypatch):
     monkeypatch.setenv(ORIGINAL_HANDLER_KEY, "no_method")
 
-    with pytest.raises(RuntimeError):
+    try:
         _handler({}, {})
+    except ValueError as e:
+        # Note: We're not using pytest.raises in order to get the exception context
+        assert "Runtime.MalformedHandlerName" in str(e)
+        assert "another exception occurred" not in traceback.format_exc()
+    else:
+        assert False
+
+
+def test_handler_not_found(monkeypatch):
+    monkeypatch.setenv(ORIGINAL_HANDLER_KEY, "sys.not_found")
+
+    try:
+        _handler({}, {})
+    except Exception as e:
+        # Note: We're not using pytest.raises in order to get the exception context
+        assert "Runtime.HandlerNotFound" in str(e)
+        assert "another exception occurred" not in traceback.format_exc()
+    else:
+        assert False
