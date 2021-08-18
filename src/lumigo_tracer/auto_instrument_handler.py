928c67e80a02904eab547aaff748fd0efd91f034
saartochner <saart@lumigo.io>
2021-08-18 16:31:51 +0300

    follow aws logic of handler importing on auto instrument. Use the runtime's logic if possible

--- src/lumigo_tracer/auto_instrument_handler.py
+++ src/lumigo_tracer/auto_instrument_handler.py
@@ -1,56 +1,34 @@
-import warnings
 import os
-import importlib
+
+try:
+    # Try to import AWS's current _get_handler logic
+    from bootstrap import _get_handler as aws_get_handler
+except Exception:
+    # Import a snapshot of _get_handler from python38 runtime
+    from lumigo_tracer.libs.bootstrap import _get_handler as aws_get_handler
 
 from lumigo_tracer import lumigo_tracer
-
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    import imp
 
 ORIGINAL_HANDLER_KEY = "LUMIGO_ORIGINAL_HANDLER"
 
 
-def parse_handler():
+def get_original_handler():
     try:
-        module_name, unit_name = os.environ[ORIGINAL_HANDLER_KEY].rsplit(".", 1)
-        file_handle, pathname, desc = imp.find_module(module_name)
+        return aws_get_handler(os.environ[ORIGINAL_HANDLER_KEY])
     except KeyError:
         raise Exception(
             "Could not find the original handler. Please contact Lumigo for more information."
         ) from None
-    except ValueError as e:
-        raise ValueError(
-            f"Runtime.MalformedHandlerName: Bad handler '{os.environ[ORIGINAL_HANDLER_KEY]}': {str(e)}"
-        ) from None
-    return module_name, unit_name, file_handle, pathname, desc
 
 
 @lumigo_tracer()
 def _handler(*args, **kwargs):
-    handler_module = ""
-    try:
-        handler_module, unit_name, file_handle, pathname, desc = parse_handler()
-        original_module = imp.load_module(handler_module, file_handle, pathname, desc)
-    except ImportError as e:
-        raise ImportError(
-            f"Runtime.ImportModuleError: Unable to import module '{handler_module}': {str(e)}"
-        ) from None
-    except SyntaxError as e:
-        raise SyntaxError(
-            f"Runtime.UserCodeSyntaxError: Syntax error in module '{handler_module}': {str(e)}"
-        ) from None
-    try:
-        original_handler = getattr(original_module, unit_name)
-    except AttributeError:
-        raise Exception(
-            f"Runtime.HandlerNotFound: Handler '{unit_name}' missing on module '{handler_module}'"
-        ) from None
+    original_handler = get_original_handler()
     return original_handler(*args, **kwargs)
 
 
 try:
     # import handler during runtime initialization, as usual.
-    importlib.import_module(parse_handler()[0])
+    get_original_handler()
 except Exception:
     pass
