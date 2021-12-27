808ebf8bb5a6aa15ca54c3a0f2f074e945f0e308
saartochner <47108628+saartochner@users.noreply.github.com>
2021-12-27 12:34:46 +0200

    deprecate enhance print (#238)
    * deprecate enhance print (no need anymore)

--- src/lumigo_tracer/user_utils.py
+++ src/lumigo_tracer/user_utils.py
@@ -3,7 +3,7 @@ import logging
 from typing import Dict, Optional
 
 from lumigo_tracer.spans_container import SpansContainer
-from lumigo_tracer.lumigo_utils import Configuration, warn_client
+from lumigo_tracer.lumigo_utils import warn_client
 
 LUMIGO_REPORT_ERROR_STRING = "[LUMIGO_LOG]"
 MAX_TAGS = 50
@@ -80,11 +80,10 @@ def log(level: int, msg: str, error_type: str, extra: Optional[Dict[str, str]]):
 
 def report_error(msg: str):
     message_with_initials = f"{LUMIGO_REPORT_ERROR_STRING} {msg}"
-    if Configuration.enhanced_print:
-        print(message_with_initials)
-    else:
-        message_with_request_id = f"RequestId: {SpansContainer.get_span().function_span.get('id')} {message_with_initials}"
-        print(message_with_request_id)
+    message_with_request_id = (
+        f"RequestId: {SpansContainer.get_span().function_span.get('id')} {message_with_initials}"
+    )
+    print(message_with_request_id)
 
 
 def validate_tag(key, value, tags_len, should_log_errors):
