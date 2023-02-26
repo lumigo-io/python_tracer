ba332f9585266fa58dc73c584b82c2152db9cbd3
saartochner <saart@lumigo.io>
2023-02-26 16:13:35 +0200

    add lumigo-core library to the layer files

--- scripts/prepare_layer_files.sh
+++ scripts/prepare_layer_files.sh
@@ -5,3 +5,4 @@ python setup.py bdist_wheel
 rm -rf python && mkdir python
 cp -R src/lumigo_tracer.egg-info python/
 cp -R src/lumigo_tracer python/
+cp -R "$(python -c "import lumigo_core; print(lumigo_core.__path__[0])")"* python/
