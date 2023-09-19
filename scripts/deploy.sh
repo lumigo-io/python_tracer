74b099709a420006e7ba4e200509a6563dd8ab61
Harel Moshe <144588679+harelmo-lumigo@users.noreply.github.com>
2023-09-19 17:23:18 +0300

    Add a Python 3.11 layer to the deploy script (#291)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,4 +21,4 @@ commit_version="$(git describe --abbrev=0 --tags)"
     --region us-west-2 \
     --package-folder python \
     --version "$commit_version" \
-    --runtimes "python3.6 python3.7 python3.8 python3.9 python3.10"
+    --runtimes "python3.6 python3.7 python3.8 python3.9 python3.10 python3.11"
