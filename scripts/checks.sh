3c1f3056f863f828087a21b0c86b76929c84464d
saartochner <saart@lumigo.io>
2023-10-09 15:08:46 +0300

    feat: add support for SQLAlchemy 1.4

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -4,4 +4,9 @@ set -eo pipefail
 pre-commit run -a
 pushd src
 py.test --all --cov=./lumigo_tracer --cov-config=.coveragerc
+
+# TODO: We should have better tests for dependencies versions
+echo "Testing sqlalchemy 1.3.16..."
+python -m pip install sqlalchemy==1.3.16 > /dev/null
+py.test -k sqlalchemy
 popd
