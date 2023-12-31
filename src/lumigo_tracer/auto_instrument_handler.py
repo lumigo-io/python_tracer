4676e85be97a43a4175be37c15efe43639fbe8f2
Sagiv Oulu <sagivo@lumigo.io>
2023-12-31 14:34:02 +0200

    fix: import new awslambdaric version based on python runtime version

--- src/lumigo_tracer/auto_instrument_handler.py
+++ src/lumigo_tracer/auto_instrument_handler.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from lumigo_tracer.lumigo_utils import is_aws_environment
 
@@ -6,8 +7,15 @@ try:
     # Try to import AWS's current _get_handler logic
     from bootstrap import _get_handler as aws_get_handler
 except Exception:
-    # Import a snapshot of _get_handler from python38 runtime
-    from lumigo_tracer.libs.bootstrap import _get_handler as aws_get_handler
+    # Import a snapshot of _get_handler from the lambda runtime interface client (awslambdaric)
+    # matching the current python version
+    if sys.version_info >= (3, 7):
+        # This version supports python 3.7 up to 3.12
+        from lumigo_tracer.libs.awslambdaric.release_2_0_8.bootstrap import _get_handler as aws_get_handler
+    else:
+        # This is the latest version that supports python 3.6, so we default to it
+        # (We don't support python 3.5 or below)
+        from lumigo_tracer.libs.awslambdaric.release_2_0_0.bootstrap import _get_handler as aws_get_handler
 
 from lumigo_tracer import lumigo_tracer
 
