a37d93bf8e8624f2d7a6fb39265131ab7e0c1538
Sagiv Oulu <sagivo@lumigo.io>
2023-12-31 16:27:18 +0200

    docs: external libs snapshots explained

--- src/lumigo_tracer/libs/__init__.py
+++ src/lumigo_tracer/libs/__init__.py
@@ -1,1 +1,11 @@
 # TODO - do we need to add to our license something that thanks those repositories?
+
+"""
+Lumigo Tracer tries to use as little external libraries as possible, but sometimes it's just not possible.
+in these cases (When the library is small enough) we copy the code to our repository and use it from there, in order to
+avoid dependency version conflict with the customers dependencies.
+
+The source code is not altered in any way, except for:
+* Imports between libraries are changed to use the local copy.
+* Auto formatting is applied.
+"""
