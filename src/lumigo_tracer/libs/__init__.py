4d40a5ac42eab700e08197b25d57d4a7f58f7916
Sagiv Oulu <sagivo@lumigo.io>
2024-01-07 10:18:01 +0200

    docs: use user and not customer

--- src/lumigo_tracer/libs/__init__.py
+++ src/lumigo_tracer/libs/__init__.py
@@ -3,7 +3,7 @@
 """
 Lumigo Tracer tries to use as little external libraries as possible, but sometimes it's just not possible.
 in these cases (When the library is small enough) we copy the code to our repository and use it from there, in order to
-avoid dependency version conflict with the customers dependencies.
+avoid dependency version conflict with the users dependencies.
 
 The source code is not altered in any way, except for:
 * Imports between libraries are changed to use the local copy.
