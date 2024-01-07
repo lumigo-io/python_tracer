98fc5cdd39bb56a9bd1d61ccbd50060227552153
Sagiv Oulu <sagivo@lumigo.io>
2024-01-07 12:23:56 +0200

    docs: fix libs snapshot explanation

--- src/lumigo_tracer/libs/__init__.py
+++ src/lumigo_tracer/libs/__init__.py
@@ -1,10 +1,7 @@
-# TODO - do we need to add to our license something that thanks those repositories?
-
 """
 Lumigo Tracer tries to use as little external libraries as possible, but sometimes it's just not possible.
 in these cases (When the library is small enough) we copy the code to our repository and use it from there, in order to
 avoid dependency version conflict with the users dependencies.
 
-The source code is not altered in any way, except for:
-* Imports between libraries are changed to use the local copy.
+The source code is not altered in any way, except for imports between libraries are changed to use the local copy.
 """
