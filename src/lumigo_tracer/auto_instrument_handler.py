af7de0d56a4ed73020cf90afdd55a892c2747cce
Sagiv Oulu <sagivo@lumigo.io>
2024-01-07 11:18:12 +0200

    docs: document why we need awslambdaric code snapshot

--- src/lumigo_tracer/auto_instrument_handler.py
+++ src/lumigo_tracer/auto_instrument_handler.py
@@ -6,7 +6,8 @@ try:
     # Try to import AWS's current _get_handler logic
     from bootstrap import _get_handler as aws_get_handler
 except Exception:
-    # Import a snapshot of _get_handler from the lambda runtime interface client (awslambdaric)
+    # Import a snapshot of _get_handler from the lambda runtime interface client (awslambdaric), in the unlikely case
+    # that the bootstrap module is not available
     from lumigo_tracer.libs.awslambdaric.release_2_0_8.bootstrap import (
         _get_handler as aws_get_handler,
     )
