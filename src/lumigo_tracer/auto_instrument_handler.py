6dd1dce83420d46e01e6143ed73e575b0d7b094e
Sagiv Oulu <sagivo@lumigo.io>
2024-01-08 13:49:46 +0200

    fix: use dedicated warn client func

--- src/lumigo_tracer/auto_instrument_handler.py
+++ src/lumigo_tracer/auto_instrument_handler.py
@@ -7,9 +7,9 @@ try:
     from bootstrap import _get_handler as aws_get_handler
 except Exception:
     # Only import the logger if we need it, no need to slow down the import time
-    from lumigo_core.logger import get_logger
+    from lumigo_tracer.lumigo_utils import warn_client
 
-    get_logger().warning(
+    warn_client(
         "Could not import built in AWS bootstrap._get_handler function. Using fallback. "
         "Please contact Lumigo for more information."
     )
