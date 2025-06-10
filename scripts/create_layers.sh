74d346f3d9168d2b0fe166c7e021a08f6f7aa05c
Guy Moses <guy@lumigo.io>
2025-06-10 15:33:12 +0300

    fix: update-layers

---
+++ scripts/create_layers.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+
+echo ".____                  .__                  .__        ";
+echo "|    |    __ __  _____ |__| ____   ____     |__| ____  ";
+echo "|    |   |  |  \/     \|  |/ ___\ /  _ \    |  |/  _ \ ";
+echo "|    |___|  |  /  Y Y  \  / /_/  >  <_> )   |  (  <_> )";
+echo "|_______ \____/|__|_|  /__\___  / \____/ /\ |__|\____/ ";
+echo "        \/           \/  /_____/         \/            ";
+echo
+echo "Update Python Tracer Layers"
+
+setup_git
+
+echo "Creating lumigo-python-tracer layer"
+./scripts/prepare_layer_files.sh
+
+echo "Creating layer latest version arn table md file (LAYERS.md)"
+commit_version="$(git describe --abbrev=0 --tags)"
+../utils/common_bash/create_layer.sh \
+    --layer-name lumigo-python-tracer \
+    --region ALL \
+    --package-folder python \
+    --version "$commit_version" \
+    --runtimes "python3.6 python3.7 python3.8 python3.9 python3.10 python3.11 python3.12 python3.13"
+
+cd .. && git clone git@github.com:lumigo-io/larn.git
+cd larn && npm i -g
+larn -r python3.6 -n layers/LAYERS36 --filter lumigo-python-tracer -p ~/python_tracer
+larn -r python3.7 -n layers/LAYERS37 --filter lumigo-python-tracer -p ~/python_tracer
+cd ../python_tracer
+git add layers/LAYERS36.md
+git add layers/LAYERS37.md
+git commit -m "layers-table: layers md [skip ci]"
+git push origin master
+
+echo "Done"
